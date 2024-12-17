ebc793399b2d46528a91726dcfea979dc984d696
Eduard0617 <edufeenandessilva0617@gmail.com>
2024-12-17 13:52:57 -0300

    criando bot para reenviar mensagens do canal teste para o ShaliahSongs

--- bot.py
+++ bot.py
@@ -3,13 +3,13 @@ from telethon import TelegramClient, events
 API_ID = '24989859'
 API_HASH = '70851b288050ecc9a5202bafda72ff77'
 TOKEN = '7462718254:AAFuRdDo3j3e9Vxhr9ZlNbDkivGygTEcbV0'
-CHANNEL_BLUNI_ID = '-1001092038063'
-CHANNEL_ID = '-1002357778672'
+CHANNEL_TESTE_ID = -1002488198462
+CHANNEL_ShaliahSongs_ID = -1002357778672
 
 bot = TelegramClient('bot', API_ID, API_HASH).start(bot_token=TOKEN)
 
-@bot.on(events.NewMessage(chats=CHANNEL_BLUNI_ID))
+@bot.on(events.NewMessage(chats=CHANNEL_TESTE_ID))
 async def message_handler(event):
-    await bot.send_message(CHANNEL_ID,event.message.message)
+    await bot.send_message(CHANNEL_ShaliahSongs_ID,event.message.message)
 
 bot.run_until_disconnected()
